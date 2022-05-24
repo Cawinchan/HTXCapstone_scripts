baadb5e4884c81d018412b814e6194f1e2f64d80
Lo Jun Kai <lojunkai98@gmail.com>
2022-05-24 15:56:35 +0800

    Refactor mlx90640 into functions
    Make it easy to call from esp32 superloop.
    Ino file is untested, might fall into errors.

---
+++ Sensorscripts/Arduino/mlx90640/setup_MLX90640.h
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+#include <string>
+
+#include "MLX90640_API.h"
+#include "MLX90640_I2C_Driver.h"
+
+const byte MLX90640_address = 0x33;  // Default 7-bit unshifted address of the MLX90640
+
+#define TA_SHIFT 8  // Default shift for MLX90640 in open air
+
+// Extern to define it as global variables
+extern float mlx90640To[768];
+extern paramsMLX90640 mlx90640;
+
+// Returns true if the MLX90640 is detected on the I2C bus
+boolean isConnected();
+
+// Converts the image to a string of integers
+std::string img_to_string(const float* mlx90640To, const int size_bytes);
+
+// Initialise the MLX90640
+void setup_MLX90640();
+
+// Sample once.
+// Image is then extracted and stored to float mlx90640To[768].
+void sample_MLX90640(float* mlx90640To);
