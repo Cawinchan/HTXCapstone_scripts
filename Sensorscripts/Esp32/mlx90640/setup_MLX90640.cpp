75fd7cc63a70b0805886163dd8c7f9fa8059dee5
Lo Jun Kai <lojunkai98@gmail.com>
2022-07-07 14:43:00 +0800

    Normalize output of mlx90640
    This is to convert float to uint8_t to save transmission bandwidth.

--- Sensorscripts/Esp32/mlx90640/setup_MLX90640.cpp
+++ Sensorscripts/Esp32/mlx90640/setup_MLX90640.cpp
@@ -1,6 +1,7 @@
 #include "setup_MLX90640.h"
 
-float mlx90640To[768];
+float mlx90640To[mlx_reading_len];  // This stores the pixel value in degree celcius
+uint8_t mlx90640_reading[mlx_reading_len];  // This stores the pixel value normalised to 0-255
 paramsMLX90640 mlx90640;
 
 void setup_MLX90640() {
@@ -24,13 +25,13 @@ void setup_MLX90640() {
 
     // Once params are extracted, we can release eeMLX90640 array
 
-    // MLX90640_SetRefreshRate(MLX90640_address, 0x02); //Set rate to 2Hz
-    //  MLX90640_SetRefreshRate(MLX90640_address, 0x03); //Set rate to 4Hz
-        MLX90640_SetRefreshRate(MLX90640_address, 0x04); //Set rate to 8Hz Most optimal speed to accuracy
-//    MLX90640_SetRefreshRate(MLX90640_address, 0x05);  // Set rate to 16Hz
+    // MLX90640_SetRefreshRate(MLX90640_address, 0x02);  // Set rate to 2Hz
+    // MLX90640_SetRefreshRate(MLX90640_address, 0x03);  // Set rate to 4Hz
+    MLX90640_SetRefreshRate(MLX90640_address, 0x04);  // Set rate to 8Hz Most optimal speed to accuracy
+    // MLX90640_SetRefreshRate(MLX90640_address, 0x05);  // Set rate to 16Hz
 }
 
-void sample_MLX90640(float* mlx90640To) {
+void sample_MLX90640(uint8_t* mlx90640_reading) {
     for (byte x = 0; x < 2; x++) {
         uint16_t mlx90640Frame[834];
         int status = MLX90640_GetFrameData(MLX90640_address, mlx90640Frame);
@@ -47,24 +48,52 @@ void sample_MLX90640(float* mlx90640To) {
         float emissivity = 0.95;
 
         MLX90640_CalculateTo(mlx90640Frame, &mlx90640, emissivity, tr, mlx90640To);
+
+        // Normalise float to uint8_t
+        // This is to safe data transmission bandwidth.
+        // This is acceptable since the image will eventually be normalised to 0-255 range
+        // in the processing of life detection.
+        // Was previously degrees celcius, so we are losing some information here.
+        auto normalise_255 = [&mlx90640_reading]() {
+            // Find max and min
+            float max_temp;
+            float min_temp;
+            for (auto i : mlx90640To) {
+                if (max_temp < i) {
+                    max_temp = i;
+                }
+                if (min_temp > i) {
+                    min_temp = i;
+                }
+            }
+            float temp_range = max_temp - min_temp;
+
+            for (int i = 0; i < mlx_reading_len; i++) {
+                // normalized_temp range = [0,1]
+                float normalized_temp = (mlx90640To[i] - min_temp) / temp_range;
+                // Round off temp by int(x + 0.5)
+                mlx90640_reading[i] = int(normalized_temp * 255.0 + 0.5);
+            }
+        };
+        normalise_255();
     }
 }
 
-String img_to_string(const float* mlx90640To, const int size_bytes) {
+String img_to_string(const uint8_t* mlx90640_reading, const int size_bytes) {
     String img_string;
 
-    const int arr_len = size_bytes / sizeof(mlx90640To[0]);
+    const int arr_len = size_bytes / sizeof(mlx90640_reading[0]);
 
     for (int i = 0; i < arr_len; i++) {
-      img_string += String(mlx90640To[i]) ;   
-      if (i != arr_len-1){
-        img_string += ",";
-      }
+        img_string += String(mlx90640_reading[i]);
+        if (i != arr_len - 1) {
+            img_string += ",";
+        }
     }
     return img_string;
 }
 
-int to_byte_array(const float* data, const int size_bytes, byte* byte_arr) {
+int to_byte_array(const uint8_t* data, const int size_bytes, byte* byte_arr) {
     memcpy(byte_arr, data, size_bytes);
     return size_bytes;
 }
--- Sensorscripts/Esp32/mlx90640/setup_MLX90640.h
+++ Sensorscripts/Esp32/mlx90640/setup_MLX90640.h
@@ -8,23 +8,24 @@ const byte MLX90640_address = 0x33;  // Default 7-bit unshifted address of the M
 
 #define TA_SHIFT 8  // Default shift for MLX90640 in open air
 
+constexpr int mlx_reading_len = 768;  // This length is determined by the sensor
 // Extern to define it as global variables
-extern float mlx90640To[768];
+extern uint8_t mlx90640_reading[mlx_reading_len];
 extern paramsMLX90640 mlx90640;
 
 // Returns true if the MLX90640 is detected on the I2C bus.
 boolean isConnected();
 
-// Converts the image to a string of floats.
-String img_to_string(const float* mlx90640To, const int size_bytes);
+// Converts the image to a string of ints.
+String img_to_string(const uint8_t* mlx90640_reading, const int size_bytes);
 
 // Convert to byte array for transmission.
 // Returns the length of byte array.
-int to_byte_array(const float* data, int arr_len, byte* byte_arr);
+int to_byte_array(const uint8_t* data, int arr_len, byte* byte_arr);
 
 // Initialise the MLX90640
 void setup_MLX90640();
 
 // Sample once.
-// Image is then extracted and stored to float mlx90640To[768].
-void sample_MLX90640(float* mlx90640To);
+// Image is then extracted and stored to int8_t mlx90640_reading[768].
+void sample_MLX90640(uint8_t* mlx90640_reading);
