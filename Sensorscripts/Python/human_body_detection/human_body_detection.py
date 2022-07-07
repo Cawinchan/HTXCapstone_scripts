c63ea19c37452eaa184f07dcfe2514ea4da2f1f8
cawinchan <cawin.chan@gmail.com>
2022-07-08 00:30:50 +0800

    add ROS functions for human body detection and standardised functions in human smell detection

---
+++ Sensorscripts/Python/human_body_detection/human_body_detection.py
@@ -0,0 +1,56 @@
+import numpy as np
+import cv2
+import serial
+from fastai.vision.all import load_learner
+
+# For windows systems
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+def td_to_img(f,tmax,tmin):
+    norm = np.uint8((f - tmin)*255/(tmax-tmin))
+    return norm
+
+def setup_human_body_detection():
+    model = load_learner('models/resnet34_person_classifier_2022.1.pkl')
+    return model
+
+def predict_human_body_detection(model):
+    ser = serial.Serial('COM4', 115200)
+    mlx_shape = (24,32)
+
+    # Read line
+    sample = ser.readline()
+    sample = sample.decode('utf-8') # decode from byte to string
+    sample = sample.split(',')
+    sample = np.array(sample).astype(np.float)
+
+    # Find the min and max values of image
+    tmax = sample.max()
+    tmin = sample.min()
+    np.nan_to_num(0)
+    sample = np.reshape(sample,mlx_shape)
+
+    # If the difference between the hottest point and the coolest point is below a threshold value
+    # make the image blank as it is highly likely to be ambient data
+    if (tmax - tmin <= 3):
+        np.full((24, 32), 0)
+
+    # Normalise image
+    tg_img = td_to_img(sample,tmin,tmax)
+
+    # Image processing to resize for model
+    # Colour grayscale image
+    img = cv2.applyColorMap(tg_img, cv2.COLORMAP_JET)
+    # Upscale Image
+    img = cv2.resize(img, (640,480), interpolation = cv2.INTER_CUBIC)
+
+    # Model prediction
+    with model.no_bar():
+        if (tmax - tmin > 3):
+            pred_class , _ , human_likelihood_prob = model.predict(img)
+        else: 
+            human_likelihood_prob = 0
+    return human_likelihood_prob
+        
--- Sensorscripts/Python/human_smell_detection/human_smell_detection.py
+++ Sensorscripts/Python/human_smell_detection/human_smell_detection.py
@@ -1,18 +1,33 @@
 # Python script for ROS interface using Serial communication for CO2
 
 # Imports 
+from email.mime import base
 import numpy as np
 import serial
 
+MAX_IAQ = 250
+IAQ_base = 50 # Inital IAQ_base value 
 
-# Model setup
-def human_breath_detector(iaq,max_iaq,base_iaq):
-    return (iaq / (max_iaq-base_iaq))
+class human_breath_detector():
+    def __init__(self,max_iaq,base_iaq):
+        self.max_iaq = max_iaq
+        self.base_iaq = base_iaq
+
+    def predict(self,iaq):
+        # Returns normalisation of iaq over range of max_iaq and base_iaq (0-100%)
+        return (iaq / (self.max_iaq-self.base_iaq))
+    
+    # Sets new base value when threshold is met to ensure that when the environment changes,
+    # the base_IAQ is calibrated correctly
+    def set_base(self, new_base_iaq):
+        self.base_iaq = new_base_iaq
+
+
+def setup_human_sound_detection():
+    model = human_breath_detector(MAX_IAQ,IAQ_base)
+    return model
 
 def predict_human_smell_detection(model):
-    MAX_IAQ = 250
-    IAQ_base = 50
-    
     ser = serial.Serial('COM4', 115200)
 
     sample = ser.readline()
@@ -20,20 +35,20 @@ def predict_human_smell_detection(model):
     sample = sample.split(',')
 
     arr = np.array(sample)
-    IAQ = float(arr[1].strip())
-    IAQ_accuracy = int(arr[2].strip())
-    IAQ_roc = float(arr[-1].replace('\r\n',''))
+    iaq = float(arr[1].strip())
+    iaq_accuracy = int(arr[2].strip())
+    iaq_roc = float(arr[-1].replace('\r\n',''))
     human_likelihood_prob = 0
-    if IAQ_accuracy < 1 and printer:
+    if iaq_accuracy < 1 and printer:
         print("IAQ Warming up")
         printer = False
-    if IAQ_accuracy > 0 and not printer:
+    if iaq_accuracy > 0 and not printer:
         print("IAQ Done Warning up")
         printer = True
     # Signal change: Update IAQ_base value
-    if IAQ_roc > 32 and IAQ_accuracy > 0:
-        IAQ_base = IAQ
-    human_likelihood_prob = (IAQ / (MAX_IAQ-IAQ_base))
+    if iaq_roc > 32 and iaq_accuracy > 0:
+        model.set_base(iaq)
+    human_likelihood_prob = model.predict(iaq)
     if human_likelihood_prob > 1:
         human_likelihood_prob = 1
     if human_likelihood_prob < 0:
